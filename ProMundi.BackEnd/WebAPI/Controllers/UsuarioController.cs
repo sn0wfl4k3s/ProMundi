c9c24aa9c58f0e144863b45bb79ec4fb5a4a31cb
Flakes <ed.afram@gmail.com>
2020-05-23 11:45:40 -0300

    Criação de controller usuário.

---
+++ ProMundi.BackEnd/WebAPI/Controllers/UsuarioController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuarioController : ControllerBase
+    {
+    }
+}
