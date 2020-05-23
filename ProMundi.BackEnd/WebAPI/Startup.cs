1f021a86a831176d153c19c046af88dce04cbbff
Flakes <ed.afram@gmail.com>
2020-05-23 17:39:14 -0300

    Inclusão de lib sqlite

--- ProMundi.BackEnd/WebAPI/Startup.cs
+++ ProMundi.BackEnd/WebAPI/Startup.cs
@@ -1,15 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using InfraData;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 
 namespace WebAPI
@@ -27,6 +22,10 @@ namespace WebAPI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            var connection = Configuration["ConexaoSqlite:SqliteConnectionString"];
+
+            services.AddDbContext<ApplicationDbContext>(options => options.UseSqlite(connection));
 
             services.AddSwaggerGen(c =>
             {
