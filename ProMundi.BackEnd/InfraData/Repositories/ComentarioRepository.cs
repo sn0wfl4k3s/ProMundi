1a9f5c151dbaf67140faf77c2c91e9aec66fa2f8
Thiago <Thiago@DESKTOP-1RR628K>
2020-05-23 16:31:50 -0300

    ajuste

---
+++ ProMundi.BackEnd/InfraData/Repositories/ComentarioRepository.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InfraData.Repositories
+{
+    public class ComentarioRepository : RepositoryBase<Comentario>
+    {
+        public ComentarioRepository(ApplicationDbContext context) : base(context)
+        {
+        }
+    }
+
+}
