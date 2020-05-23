55d62faecd546fb171d50a3a5f15c01454501aea
Thiago <Thiago@DESKTOP-1RR628K>
2020-05-23 17:37:41 -0300

    controller base e autoMapper

---
+++ ProMundi.BackEnd/WebAPI/Controllers/Base/CrudControllerBaseController.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Domain.Base;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers.Base
+{
+    public class CrudControllerBase<T, TViewModelCadastro, TViewModel> :
+        ControllerBase
+        where T : class
+    {
+        private readonly IRepositoryBase<T> _repositoryBase;
+        private readonly IMapper _mapper;
+
+        public CrudControllerBase(IRepositoryBase<T> repositoryBase, IMapper mapper)
+        {
+            _repositoryBase = repositoryBase;
+            _mapper = mapper;
+        }
+
+        [AllowAnonymous]
+        [HttpGet]
+        public virtual async Task<IActionResult> GetAllAsync()
+        {
+            var objectToGet = await _repositoryBase.GetAllAsync();
+            return Ok(_mapper.Map<List<TViewModel>>(objectToGet));
+        }
+
+        [AllowAnonymous]
+        [HttpGet("{id}")]
+        public virtual IActionResult GetById(int id)
+        {
+            var objectToGet = _repositoryBase.GetById(id);
+
+            return Ok(_mapper.Map<TViewModel>(objectToGet));
+        }
+
+        [HttpPost]
+        public virtual async Task<IActionResult> Create([FromBody] TViewModelCadastro model)
+        {
+            var objectToCreate = _mapper.Map<TViewModelCadastro, T>(model);
+
+            await _repositoryBase.AddAsync(objectToCreate);
+
+            return StatusCode(201);
+        }
+
+        [HttpPut("{id}")]
+        public virtual async Task<IActionResult> Update([FromBody] TViewModel model)
+        {
+            var objectToUpdate = _mapper.Map<TViewModel, T>(model);
+
+            await _repositoryBase.Update(objectToUpdate);
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public virtual async Task<IActionResult> Delete(int id)
+        {
+            //if (id == null)
+            //    return BadRequest();
+
+            await _repositoryBase.Delete(id);
+
+            return NoContent();
+        }
+    }
+}
--- ProMundi.BackEnd/WebAPI/Startup.cs
+++ ProMundi.BackEnd/WebAPI/Startup.cs
@@ -11,6 +11,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using AutoMapper;
 
 namespace WebAPI
 {
@@ -26,6 +27,8 @@ namespace WebAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors();
+            services.AddAutoMapper(typeof(Startup));
             services.AddControllers();
 
             services.AddSwaggerGen(c =>
