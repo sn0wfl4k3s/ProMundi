30fbc7f560c549deb92ff8659e7215326d286b74
Thiago <Thiago@DESKTOP-1RR628K>
2020-05-23 17:47:57 -0300

    view models

---
+++ ProMundi.BackEnd/WebAPI/ViewModels/Categoria/CategoriaViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAPI.ViewModels.Conteudo;
+using WebAPI.ViewModels.Usuario;
+
+namespace WebAPI.ViewModels.Categoria
+{
+    public class CategoriaViewModel
+    {
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+
+        public IList<UsuarioViewModel> Usuarios { get; set; }
+        public IList<ConteudoViewModel> Conteudos { get; set; }
+    }
+}
