073240f2d671f8df13d41c3831b257f3e7b40693
Thiago <Thiago@DESKTOP-1RR628K>
2020-05-23 22:38:10 -0300

    reset css

--- ProMundi.FrontEnd/src/app/app.module.ts
+++ ProMundi.FrontEnd/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { PrincipalModule } from './principal/principal/principal.module';
+import { PrincipalModule } from './principal/principal.module';
 import { SharedModule } from './shared/shared.module';
 
 @NgModule({
--- ProMundi.FrontEnd/src/app/principal/principal.module.ts
+++ ProMundi.FrontEnd/src/app/principal/principal.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HomeComponent } from '../home/home.component';
-import { HeaderComponent } from '../header/header.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
 import { SharedModule } from 'src/app/shared/shared.module';
 
 @NgModule({
